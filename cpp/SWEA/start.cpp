dd24ce84abb80a082724c503dfbac8a1fc4ef940
pingu9 <djg4053@gmail.com>
2022-02-24 20:34:38 +0900

    added start code

---
+++ cpp/SWEA/start.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int T, N;
+    cin >> T;
+
+    for (int test_case = 1; test_case <= T; test_case++) {
+        int result = 0;
+
+
+        cout << '#' << test_case << ' ' << result << '\n';
+    }
+
+
+}
