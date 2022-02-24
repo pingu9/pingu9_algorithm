54d10fff409ccdc2c547c59866b8ec5741d56b24
pingu9 <djg4053@gmail.com>
2022-02-24 19:37:25 +0900

    edit start code

--- JAVA/SWEA/Start.java
+++ JAVA/SWEA/Start.java
@@ -5,10 +5,16 @@ import java.io.InputStreamReader;
 
 public class Start {
 
+    static int T;
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= T; testCase ++) {
+            int result = 0;
 
-        
+            System.out.printf("#%d %d", testCase, result);
+        }
 
     }
 
