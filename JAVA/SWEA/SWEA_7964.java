19317a1247dd7967e2a1732a434d7614fbd13811
pingu9 <djg4053@gmail.com>
2022-02-24 19:27:17 +0900

    added start code

---
+++ JAVA/SWEA/SWEA_7964.java
@@ -0,0 +1,15 @@
+package JAVA.SWEA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SWEA_7964 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+
+    }
+
+}
