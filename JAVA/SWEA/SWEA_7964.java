24ca82d4578f2f543b75d99b1219a31638ea22f3
pingu9 <djg4053@gmail.com>
2022-02-24 19:36:16 +0900

    edit start code

--- JAVA/SWEA/SWEA_7964.java
+++ JAVA/SWEA/SWEA_7964.java
@@ -5,9 +5,17 @@ import java.io.InputStreamReader;
 
 public class SWEA_7964 {
 
+    static int N, D, T;
+
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        T = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= T; testCase ++) {
+            int result = 0;
+
+            System.out.printf("#%d %d", testCase, result);
+        }
         
 
     }
