e0e1d7bb9fdffbfb39531b34aae47129f0973f00
pingu9 <djg4053@gmail.com>
2022-03-03 00:22:00 +0900

    finished BOJ_1713: 후보 추천하기

--- cpp/BOJ/1713/BOJ_1713.cpp
+++ cpp/BOJ/1713/BOJ_1713.cpp
@@ -4,18 +4,28 @@
 
 using namespace std;
 
-int recommend[101];
+pair<int, int> recommend[101];
 
 int candidate_num = 0;
 
 int find_min_pos()
 {
     int mn = 99999999;
+    int order_min = 99999999;
     int pos = 0;
     for (int i = 1; i <= 100; i++) {
-        if (recommend[i] < mn) {
-            mn = recommend[i];
-            pos = i;
+        if (recommend[i].first) {
+            if (recommend[i].first < mn) {
+                mn = recommend[i].first;
+                order_min = recommend[i].second;
+                pos = i;
+            }
+            else if (recommend[i].first == mn) {
+                if (recommend[i].second < order_min) {
+                    order_min = recommend[i].second;
+                    pos = i;
+                }
+            }
         }
     }
 
@@ -30,30 +40,34 @@ int main()
     int N, T;
     cin >> N >> T;
 
+    int order = 1;
+
     for (int i = 0; i < T; i++) {
         int cur = 0;
         cin >> cur;
 
-        if (recommend[cur]) {
-            recommend[cur]++;
+        if (recommend[cur].first) {
+            recommend[cur].first++;
         }
         else if (candidate_num == N) {
             int pos = find_min_pos();
-            recommend[pos] = 0;
-            recommend[cur] = 1;
+            recommend[pos].first = 0;
+            recommend[cur] = make_pair(1, order++);
         }
         else {
-            recommend[cur] = 1;
+            recommend[cur] = make_pair(1, order++);
             candidate_num++;
         }
         
     }
 
-    for (int i = 1; i <= N; i++) {
-        if (recommend[i]) {
+    int count = N - 1;
+    for (int i = 1; i <= 100; i++) {
+        if (recommend[i].first) {
             cout << i;
-            if (i != N) {
+            if (count) {
                 cout << ' ';
+                count--;
             }
         }
         
